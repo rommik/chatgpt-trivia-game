8f2c1125ad00097bc995aa96c7da87fa76c1151d
Roman Mikhailov <rommik@gmail.com>
2023-09-09 19:39:10 -0400

    created basic trivia layout and functionality

--- app.js
+++ app.js
@@ -3,12 +3,38 @@ const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
+app.use(express.json()); // Add JSON body parser middleware
 
-app.get('/api/', (req, res) => {
-  res.send('Hello World!')
+// Define your dictionary of questions and answers
+const questionsDictionary = {
+    1: {id:1, question: 'What is the capital of France?', answer: 'Paris' },
+    2: { id:2, question: 'Who wrote "Romeo and Juliet"?', answer: 'William Shakespeare' },
+    // Add more questions and answers as needed
+};
+
+app.get('/api/getQuestion', (req, res) => {
+    const questionIds = Object.keys(questionsDictionary);
+    const randomQuestionId = questionIds[Math.floor(Math.random() * questionIds.length)];
+    const randomQuestion = questionsDictionary[randomQuestionId];
+    res.json(randomQuestion);
 });
+
+const userAnswers ={
+    1: {isCorrect: true},
+    2: {isCorrect: false},
+}
+    
+app.post('/api/answerQuestion', (req, res) => {
+    const questionId = req.body.questionId;
+    const userAnswer = req.body.userAnswer;
+    console.log(questionId, userAnswers);
+    userAnswers[questionId] = userAnswer;
+    res.json();
+});
+
+
 
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`)
-    }
+}
 );
--- public/main.js
+++ public/main.js
@@ -1,9 +1,74 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const card = document.querySelector('.card-inner');
-    const flipButton = document.getElementById('flip-button');
-  
-    flipButton.addEventListener('click', () => {
-      card.classList.toggle('is-flipped');
+    document.querySelector('.card-inner').addEventListener('transitionend', function () {
+        console.log('rotation ended');
+        if (this.style.transform === 'rotateY(180deg)') {
+            document.querySelector('.feedback').style.display = 'flex';
+        } else {
+            document.querySelector('.feedback').style.display = 'none';
+        }
     });
-  });
-  
+
+    document.querySelector('.card-inner').addEventListener('click', function () {  
+        if (this.style.transform !== 'rotateY(180deg)')      
+            toggleCard(this);
+
+    });
+
+    document.getElementById('smile-button').addEventListener('click', function () {
+        postAnswer(document.querySelector('.feedback-question-id').innerHTML, true);
+        const cardInner = document.querySelector('.card-inner');
+        toggleCard(cardInner);
+    });
+    document.getElementById('frown-button').addEventListener('click', function () {
+        postAnswer(document.querySelector('.feedback-question-id').innerHTML, false);
+        const cardInner = document.querySelector('.card-inner');
+        toggleCard(cardInner);
+
+    });
+
+    
+    // Function to fetch a random question from the server
+    const fetchQuestion = () => {
+        const questionText = document.querySelector('.card-front');
+        const answerText = document.querySelector('.card-back');
+        const hiddenQuestionId = document.querySelector('.feedback-question-id');
+        fetch('/api/getQuestion')
+            .then((response) => response.json())
+            .then((data) => {
+                questionText.innerText = data.question;
+                answerText.innerText = data.answer;
+                hiddenQuestionId.innerText = data.id;
+            })
+            .catch((error) => console.error('Error fetching question:', error));
+    };
+
+    const postAnswer = (questionId, userAnswer) => {
+        console.log(questionId, userAnswer);
+        fetch('/api/answerQuestion', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ questionId, userAnswer }),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log('Success:', data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    };
+
+    // Initial fetch of a random question
+    fetchQuestion();
+
+
+});
+
+
+function toggleCard(cardInner) {
+    console.log(cardInner);
+    cardInner.style.transform = cardInner.style.transform === 'rotateY(0deg)' ? 'rotateY(180deg)' : /*do nothing*/ 'rotateY(0deg)';
+}
+
